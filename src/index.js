b46443a2ece543027df8f1966880696281f41940
Arthur Pedroti <arthurpedroti@gmail.com>
2022-07-22 09:16:05 -0300

    feat: change ga implementation and add title to pages

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import GA4React from 'ga-4-react';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
-const ga4react = new GA4React(process.env.REACT_APP_GA_TRACKING);
-
-(async () => {
-  await ga4react
-    .initialize()
-    .then(() => console.log('Analytics Success.'))
-    .catch(() => console.log('Analytics Failure.'))
-    .finally(() => {
-      ReactDOM.render(
-        <>
-          <App />
-        </>,
-        document.getElementById('root'),
-      );
-    });
-})();
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.register({
   onUpdate: registration => {
--- src/routes.js
+++ src/routes.js
@@ -17,30 +17,146 @@ import Success from './pages/Success';
 import DifferentialRateCalculator from './pages/DifferentialRateCalculator';
 
 import AsyncNoMatch from './pages/NoMatch';
+import Page from './components/Page';
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/sellerdetails" component={SellerDetails} />
-        <Route exact path="/clientdetails" component={ClientDetails} />
-        <Route exact path="/clientregister" component={ClientRegister} />
-        <Route exact path="/productdetails" component={ProductDetails} />
-        <Route exact path="/orderoptions" component={OrderOptions} />
-        <Route exact path="/orderdetails" component={OrderDetails} />
-        <Route exact path="/paymentdetails" component={PaymentDetails} />
-        <Route exact path="/freightdetails" component={FreightDetails} />
-        <Route exact path="/otherdetails" component={OtherDetails} />
-        <Route exact path="/orderslist" component={OrdersList} />
-        <Route exact path="/confirm" component={Confirm} />
-        <Route exact path="/success" component={Success} />
+        <Route
+          exact
+          path="/"
+          render={props => (
+            <Page title="Login | Pv AGF">
+              <Login {...props} />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/sellerdetails"
+          render={props => (
+            <Page title="Informações do Vendedor | Pv AGF | Pv AGF">
+              <SellerDetails {...props} />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/clientdetails"
+          render={props => (
+            <Page title="Informações do Cliente | Pv AGF">
+              <ClientDetails {...props} />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/clientregister"
+          render={props => (
+            <Page title="Cadastro de Clientes | Pv AGF">
+              <ClientRegister {...props} />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/productdetails"
+          render={props => (
+            <Page title="Informações dos Produtos | Pv AGF">
+              <ProductDetails {...props} />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/orderoptions"
+          render={props => (
+            <Page title="Tipo de Pedido | Pv AGF">
+              <OrderOptions {...props} />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/orderdetails"
+          render={props => (
+            <Page title="Detalhes do Pedido | Pv AGF">
+              <OrderDetails {...props} />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/paymentdetails"
+          render={props => (
+            <Page title="Detalhes do Pagamento | Pv AGF">
+              <PaymentDetails {...props} />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/freightdetails"
+          render={props => (
+            <Page title="Informações do Frete | Pv AGF">
+              <FreightDetails {...props} />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/otherdetails"
+          render={props => (
+            <Page title="Outras informações | Pv AGF">
+              <OtherDetails {...props} />
+            </Page>
+          )}
+        />
+
+        <Route
+          exact
+          path="/confirm"
+          render={props => (
+            <Page title="Confirme as informações | Pv AGF">
+              <Confirm {...props} />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/success"
+          render={props => (
+            <Page title="Gerar PDF | Pv AGF">
+              <Success {...props} />
+            </Page>
+          )}
+        />
+        <Route
+          exact
+          path="/orderslist"
+          render={props => (
+            <Page title="Pedidos | Pv AGF">
+              <OrdersList {...props} />
+            </Page>
+          )}
+        />
         <Route
           exact
           path="/differential-rate-calculator"
-          component={DifferentialRateCalculator}
+          render={props => (
+            <Page title="Diferencial de Alíquota | Pv AGF">
+              <DifferentialRateCalculator {...props} />
+            </Page>
+          )}
         />
-        <Route component={AsyncNoMatch} />
+        <Route
+          render={props => (
+            <Page title="Página não encontrada | Pv AGF">
+              <AsyncNoMatch {...props} />
+            </Page>
+          )}
+        />
       </Switch>
     </div>
   );
